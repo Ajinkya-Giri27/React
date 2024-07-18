f28db4080de96278dd59475d293f687e5a62eb4e
Ajinkya-Giri27 <giriajinkya02@gmail.com>
2024-07-18 14:25:14 +0530

    jsx

--- README.md
+++ README.md
@@ -19,3 +19,12 @@ This repo is for React practice
 - error handling
 - tree shaking - remove unused code0
 - Different dev and production bundle
+
+# jsx (Javascript xml)
+
+- jsx is html like syntax
+- jsx is not html
+- jsx is syntax extension for javascript
+- you can write html like code directly in javascript
+- jsx and react are two differen things
+- jsx is used with react to describe UI
--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  { id: "aj", xyz: "abc" },
-  "Hello world using react"
-);
+const heading = createElement("h1", { id: "headind" }, "hello react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
 
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
-
-root1.render(heading1);
-
-// const btn1 = React.createElement("button", {}, "submit");
-// root1.render(btn1);
-
-// console.log(heading1);
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "yo i'm child"),
-    React.createElement("h2", {}, "i'm sibling of childhsdbJHBJHJb"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "yo i'm child"),
-    React.createElement("h2", {}, "i'm sibling of child"),
-  ]),
-]);
-
-console.log(parent);
-root1.render(parent);
-
-// React.createElement("div", { id: "child" }, [
-//   React.createElement("h1", {}, "yo i'm child"),
-//   React.createElement("h2", {}, "i'm sibling of child"),
-// ]),
+const jsxheading = <h1 id="jsxh1">Hello world using jsx</h1>;
